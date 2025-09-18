68651f8eaef10b4e7cc95c7c3bc0a692bedbc5b8
Faythe2324 <faythemagsombol23@gmail.com>
2025-09-18 23:34:51 +0800

    Implemented deposit validation and fixed Bank.computeInterest

---
+++ Lab 3/bank.py
@@ -0,0 +1,96 @@
+import pickle
+import random
+from savingsaccount import SavingsAccount
+
+class Bank:
+    """This class represents a bank as a collection of savings accounts."""
+
+    def __init__(self, fileName = None):
+        self.accounts = {}
+        self.fileName = fileName
+        if fileName is not None:
+            fileObj = open(fileName, 'rb')
+            while True:
+                try:
+                    account = pickle.load(fileObj)
+                    self.add(account)
+                except Exception:
+                    fileObj.close()
+                    break
+
+    def __str__(self):
+        return "\n".join(map(str, self.accounts.values()))
+
+    def makeKey(self, name, pin):
+        return name + "/" + pin
+
+    def add(self, account):
+        key = self.makeKey(account.getName(), account.getPin())
+        self.accounts[key] = account
+
+    def remove(self, name, pin):
+        key = self.makeKey(name, pin)
+        return self.accounts.pop(key, None)
+
+    def get(self, name, pin):
+        key = self.makeKey(name, pin)
+        return self.accounts.get(key, None)
+
+    def computeInterest(self):
+        total = 0
+        for account in self.accounts.values():
+            total += account.computeInterest()
+        return total
+
+    def getKeys(self):
+        return sorted(self.accounts.keys())
+
+    def save(self, fileName = None):
+        if fileName is not None:
+            self.fileName = fileName
+        elif self.fileName is None:
+            return
+        fileObj = open(self.fileName, 'wb')
+        for account in self.accounts.values():
+            pickle.dump(account, fileObj)
+        fileObj.close()
+
+
+# ---- Test Functions ----
+
+def createBank(numAccounts = 1):
+    names = ("Brandon", "Molly", "Elena", "Mark", "Tricia",
+             "Ken", "Jill", "Jack")
+    bank = Bank()
+    upperPin = numAccounts + 1000
+    for pinNumber in range(1000, upperPin):
+        name = random.choice(names)
+        balance = float(random.randint(100, 1000))
+        bank.add(SavingsAccount(name, str(pinNumber), balance))
+    return bank
+
+def testAccount():
+    account = SavingsAccount("Ken", "1000", 500.00)
+    print(account)
+    print(account.deposit(100))
+    print("Expect 600:", account.getBalance())
+    print(account.deposit(-50))
+    print("Expect 600:", account.getBalance())
+    print(account.withdraw(100))
+    print("Expect 500:", account.getBalance())
+    print(account.withdraw(-50))
+    print("Expect 500:", account.getBalance())
+    print(account.withdraw(100000))
+    print("Expect 500:", account.getBalance())
+
+def main(number = 10, fileName = None):
+    testAccount()
+    if fileName:
+        bank = Bank(fileName)
+    else:
+        bank = createBank(number)
+    print("\nBank Accounts:\n")
+    print(bank)
+
+if __name__ == "__main__":
+    main()
---
+++ Lab 3/savingsaccount.py
@@ -0,0 +1,47 @@
+class SavingsAccount:
+    """This class represents a savings account
+    with the owner's name, PIN, and balance."""
+
+    RATE = 0.02    # Single rate for all accounts
+
+    def __init__(self, name, pin, balance = 0.0):
+        self.name = name
+        self.pin = pin
+        self.balance = balance
+
+    def __str__(self):
+        """Returns the string rep."""
+        result =  'Name:    ' + self.name + '\n' 
+        result += 'PIN:     ' + self.pin + '\n' 
+        result += 'Balance: ' + str(self.balance)
+        return result
+
+    def getBalance(self):
+        return self.balance
+
+    def getName(self):
+        return self.name
+
+    def getPin(self):
+        return self.pin
+
+    def deposit(self, amount):
+        """If valid, adds it to balance; else error msg."""
+        if amount < 0:
+            return "Amount must be >= 0"
+        self.balance += amount
+        return None
+
+    def withdraw(self, amount):
+        if amount < 0:
+            return "Amount must be >= 0"
+        elif self.balance < amount:
+            return "Insufficient funds"
+        else:
+            self.balance -= amount
+            return None
+
+    def computeInterest(self):
+        interest = self.balance * SavingsAccount.RATE
+        self.deposit(interest)
+        return interest
