6154d5c0878d1e9347c3e91b8e6b89846141a81d
Faythe2324 <faythemagsombol23@gmail.com>
2025-09-24 22:26:23 +0800

    get name of txt file

---
+++ Lab 4/PostProb_2/test.py
@@ -0,0 +1,18 @@
+#updated: get the name of a text file
+import tkinter as tk
+from tkinter import filedialog  
+import os
+
+def get_filename():
+    root = tk.Tk()
+    root.withdraw() 
+    filename = filedialog.askopenfilename(
+        title="Select a txt file",
+        filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
+    )
+    basename = os.path.basename(filename)
+    print("Selected file:", basename)
+    return filename
+
+if __name__ == "__main__":
+    get_filename()
