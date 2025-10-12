d25ee24d7eafb506a83e1315abbf63239204fdfb
Faythe2324 <faythemagsombol23@gmail.com>
2025-10-13 01:38:59 +0800

    checked sm

--- Lab 3/savingsaccount.py
+++ Lab 3/savingsaccount.py
@@ -2,7 +2,7 @@ class SavingsAccount:
     """This class represents a savings account
     with the owner's name, PIN, and balance."""
 
-    RATE = 0.02    # Single rate for all accounts
+    RATE= 0.02    # Single rate for all accounts
 
     def __init__(self, name, pin, balance = 0.0):
         self.name = name
@@ -26,7 +26,7 @@ class SavingsAccount:
         return self.pin
 
     def deposit(self, amount):
-        """If valid, adds it to balance; else error msg."""
+        """If valid, adds it to balance; else error message."""
         if amount < 0:
             return "Amount must be >= 0"
         self.balance += amount
